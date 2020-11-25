9171fa40fb15ae07e29c625a289d88550a34c29f
AnaMariaBiliciuc <70842937+AnaMariaBiliciuc@users.noreply.github.com>
2020-11-25 12:04:21 +0300

    Add files via upload

---
+++ Untitled-1222.py
@@ -0,0 +1,6 @@
+s1=str(input("Introduceti primul cuvant exprimat printro variabila: "))
+s2=str(input("Introduceti a2 cuvant exprimat printro variabila: " ))
+s3=str(input("Introduceti a3 cuvant exprimat printro variabila: "))
+s4=str(input("Introduceti a4 cuvant exprimat printro variabila: "))
+print("Fraza formata din aceste cuvinte este : {0} {1} {2} {3}" .format(s1, s2, s3, s4 ),".")
+
